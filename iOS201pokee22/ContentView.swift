d1b21fc8d3eef1a4b9eb972b8da3c5fe37bdb52f
mrbin22 <23cm0122@jec.ac.jp>
2024-04-27 10:13:13 +0900

    Initial Commit

---
+++ iOS201pokee22/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS201pokee22
+//
+//  Created by cmStudent on 2024/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS201pokee22/iOS201pokee22App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS201pokee22App.swift
+//  iOS201pokee22
+//
+//  Created by cmStudent on 2024/04/27.
+//
+
+import SwiftUI
+
+@main
+struct iOS201pokee22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
